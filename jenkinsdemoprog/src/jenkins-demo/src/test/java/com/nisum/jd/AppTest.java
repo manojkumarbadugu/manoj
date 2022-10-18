4c05f0a4ce943126fc871903d5f2fdd3a0975fd4
manojkumarbadugu <111864887+manojkumarbadugu@users.noreply.github.com>
2022-10-18 12:14:42 +0530

    Add files via upload
    adding jenkins file

---
+++ jenkinsdemoprog/src/jenkins-demo/src/test/java/com/nisum/jd/AppTest.java
@@ -0,0 +1,20 @@
+package com.nisum.jd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
